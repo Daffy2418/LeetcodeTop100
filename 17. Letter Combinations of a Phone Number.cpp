6f12ddd955d93a154eb49f441b87d5622815de5c
Daffy Liu <llrtime@bupt.edu.cn>
2017-08-29 21:53:41 +0800

    Create 17. Letter Combinations of a Phone Number.cpp

---
+++ 17. Letter Combinations of a Phone Number.cpp
@@ -0,0 +1,28 @@
+/*
+*DFS
+*/
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {    
+        vector<string> res;
+        if(digits.size()==0) return res;
+        string tmpres(digits.size(), ' ');
+        dfs(digits, 0, tmpres, res);
+        return res;
+    }
+     
+    void dfs(const string &digits, int index, string &tmpres, vector<string>&res)
+    {
+        string numap[] = {" ","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        if(index == digits.size())
+        {
+            res.push_back(tmpres);
+            return;
+        }
+        for(int i = 0; i < numap[digits[index] - '0'].size(); i++)
+        {
+            tmpres[index] = numap[digits[index] - '0'][i];
+            dfs(digits, index+1, tmpres, res);
+        }
+    }
+};
