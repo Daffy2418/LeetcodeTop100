3463e17fcd2e082b5d62c02e81eb7bdf033c8242
Daffy Liu <llrtime@bupt.edu.cn>
2017-09-21 21:26:53 +0800

    Create 263. Ugly number.cpp

---
+++ 263. Ugly number.cpp
@@ -0,0 +1,13 @@
+/*
+判断一个数的质数因子是否只有2,3,5.方法是一直除，然后观察最后是否为1
+*/
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num <= 0) return false;
+        while (num % 2 == 0) num /= 2;
+        while (num % 3 == 0) num /= 3;
+        while (num % 5 == 0) num /= 5;
+        return num == 1;
+    }
+};
